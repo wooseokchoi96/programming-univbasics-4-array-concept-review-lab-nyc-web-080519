58896e7fdb732bda0afcebe3ca5ff218369885e8
Andy Choi <wooseokchoi96@gmail.com>
2019-07-11 20:37:15 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,7 +1,9 @@
 def find_element_index(array, value_to_find)
   array.length.times{|index|
-  break if array[index] == value_to_find
+  if array[index] == value_to_find
     index
+    break
+  end
   }
 end
 
