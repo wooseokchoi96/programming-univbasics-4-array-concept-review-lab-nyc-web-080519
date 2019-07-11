00e2d857efbb1bb7795f94e1db8167b658e7cf54
Andy Choi <wooseokchoi96@gmail.com>
2019-07-11 20:27:24 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,7 +1,7 @@
 def find_element_index(array, value_to_find)
   array.length.times{|index|
   if array[index] == value_to_find
-    puts index
+    index
     break
   end
   }
@@ -14,7 +14,7 @@ def find_max_value(array)
       current_max = array[index]
     end
   }
-  puts current_max
+  current_max
 end
 
 def find_min_value(array)
@@ -24,5 +24,5 @@ def find_min_value(array)
       current_min = array[index]
     end
   }
-  puts current_min
+  current_min
 end
